dd43d23ae93c5f36810d6c743ef55cb6e6849b2a
Mauro Monereo <mauromonereo@MacBook-Pro-de-Mauro.local>
2021-01-28 16:29:34 +0100

    test

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: mmonereo <mmonereo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 11:38:16 by mmonereo          #+#    #+#             */
-/*   Updated: 2021/01/27 10:41:30 by mmonereo         ###   ########.fr       */
+/*   Updated: 2021/01/28 16:27:02 by mmonereo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int ft_printf(const char *format, ...);
 
@@ -20,51 +21,54 @@ int main (void)
 	int mine;
 	int org;
 	
-	// // 						STRING
-	// char *sapo = "chimpacho";
-	// mine = ft_printf("yo: %7.4s, capullo, %-12.12s, calamita\n", sapo, "acuña");
-	// org = printf("og: %7.4s, capullo, %-12.12s, calamita\n", sapo, "acuña");
+	// // // 						STRING
+	// char *ss = "chimpacho";
+	// mine = ft_printf("yo: %7.4s, capullo, %-12.12s, calamita\n", ss, "acuña");
+	// org = printf("og: %7.4s, capullo, %-12.12s, calamita\n", ss, "acuña");
 	// printf ("mine: %i,   org: %i\n", mine, org);
 
-	// // // 						CHAR
-	// char *sapo = "chimpacho";
-	// mine = ft_printf("yo: %7c, capullo, %-12c, calamita\n", sapo[0], '\0');
-	// org = printf("og: %7c, capullo, %-12c, calamita\n", sapo[0], '\0');
+	// // // // 						CHAR
+	// char *s = "chimpacho";
+	// mine = ft_printf("yo: %7c, capullo, %-12c, calamita\n", s[0], '\0');
+	// org = printf("og: %7c, capullo, %-12c, calamita\n", s[0], '\0');
 	// printf ("mine: %i,   org: %i\n", mine, org);
 
-							// INT
-	// int var = 33;
-	// mine = ft_printf("yo: %*i, %5.0d, 66\n", 6, var, 0);
-	// 	org = printf("og: %*i, %5.0d, 66\n", 6, var, 0);
+	// 						// INT
+	// int var1 = 33;
+	// mine = ft_printf("yo: %*i, %5.0d, 66\n", 6, var1, 0);
+	// 	org = printf("og: %*i, %5.0d, 66\n", 6, var1, 0);
 	// printf ("mine: %i,   org: %i\n", mine, org);
 
-					// UNSIGNED INT
-	// int var = 33;
-	// mine = ft_printf("yo: %*u, %5.0u, 66\n", 6, var, -42);
-	// 	org = printf("og: %*u, %5.0u, 66\n", 6, var, -42);
+	// 				// UNSIGNED INT
+	// int var2 = 33;
+	// mine = ft_printf("yo: %*u, %5.0u, 66\n", 6, var2, -42);
+	// 	org = printf("og: %*u, %5.0u, 66\n", 6, var2, -42);
 	// printf ("mine: %i,   org: %i\n", mine, org);
 
-							// HEX
-	int var = 457;
-	mine = ft_printf("yo: %.6x, %-15X\n",var, -322);
-		org = printf("og: %.6x, %-15X\n",var, -322);
+	// 						// HEX
+	// int var3 = 457;
+	// mine = ft_printf("yo: %.6x, %-15X\n",var3, -322);
+	// 	org = printf("og: %.6x, %-15X\n",var3, -322);
+	// printf ("mine: %i,   org: %i\n", mine, org);
+
+	// 					//	POINTER
+	int *var4 = 0;
+	// char c = 'f';
+	mine = ft_printf("yo: %20p, %-14p\n",&var4, NULL);
+		org = printf("og: %20p, %-14p\n",&var4, NULL);
 	printf ("mine: %i,   org: %i\n", mine, org);
 
-						//	POINTER
-	// int var = 457;
-	// char c = 'f';
-	// mine = ft_printf("yo: %20p, %-14p\n",&var, &c);
-	// 	org = printf("og: %20p, %-14p\n",&var, &c);
-	// printf ("mine: %i,   org: %i\n", mine, org);
-
-		// // 						PERCENT
+	// 	// // 						PERCENT
 	// mine = ft_printf("yo: %7%, %012%, calamita\n");
 	// org = printf("og: %7%, %012%, calamita\n");
 	// printf ("mine: %i,   org: %i\n", mine, org);
 
 							// INT	MAX
-	// int var = -2147483648;
-	// mine = ft_printf("yo: %030d, 66\n", var);
-	// 	org = printf("og: %030d, 66\n", var);
+	// int var5 = -2147483648;
+	// mine = ft_printf("yo: %030d, 66\n", var5);
+	// 	org = printf("og: %030d, 66\n", var5);
 	// printf ("mine: %i,   org: %i\n", mine, org);
+
+	// system("leaks a.out");
+	// getchar();
 }
